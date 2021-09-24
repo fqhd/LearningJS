e83ef729abb3f7a38e887406d3c6f2460bd2d5d2
fqhd <fahd31256@gmail.com>
2021-09-24 18:21:41 +0100

    Concatinating arrays

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 'use strict';
 
-let object = {w: 8000, yourOtherVariable: 9, get weight() {return this.w}}
-object.myVariable = 88;
-Object.defineProperty(object, 'z', { value: 9, writtable: false });
-Object.defineProperties(object, {
-	x: {
-		value: undefined,
-	},
-	y: {
-		value: 10,
-	}
-});
+const parents = ['mary', 'sam'];
+const children = ['stale', 'john', 'robert'];
+const family = parents.concat(children);
 
-console.log(object.x);
-helloWorld(object, object.y);
-
-function helloWorld(a){
-	console.log(a.y);
+let sum = () => {
+	return 9;
 }
+
+let concat = () => {
+	return [].concat(parents, children);
+}
+
+(() => {
+	console.log(sum());
+	console.log(concat());
+})();
