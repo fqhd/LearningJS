f178b7934f8014cd1fc665082f0e80967c73ec49
fqhd <fahd31256@gmail.com>
2021-09-24 19:52:54 +0100

    BigInt

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-const person = {
-	name: 'John',
-	age: 22
-};
-
-const handler = {
-	get: function(target, property, receiving){
-		return 'world';
-	}
-};
-
-const proxy = new Proxy(person, handler);
-
-console.log(proxy.age);
+let bigNumber = Number.MAX_SAFE_INTEGER;
+console.log('My Number: ' + bigNumber);
+bigNumber += 100000000000000000000000000093849352032903850200010000000000000000000000000009384935203290;
+console.log('My Number: ' + bigNumber);
