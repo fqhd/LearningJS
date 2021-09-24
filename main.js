6777a125a9b6941ed44421927d2551a673f6feeb
fqhd <fahd31256@gmail.com>
2021-09-24 21:06:20 +0100

    Generator with parameters

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-function* generator(){
+function* generator(value){
 	let myArray = [1, 2, 3];
+	yield value;
 	yield myArray[0];
 	yield myArray[1];
 	yield myArray[2];
 }
 
-let myIterator = generator();
+let myIterator = generator('ooga booga');
 
+console.log(myIterator.next());
 console.log(myIterator.next());
 console.log(myIterator.next());
 console.log(myIterator.next());
