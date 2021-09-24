720b693813f9b507642bf751f292acf6023a006d
fqhd <fahd31256@gmail.com>
2021-09-24 22:10:29 +0100

    Strict mode classes

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const car = {
-	vroom: () => {
-		console.log('Vroom!!!!');
+class Cat {
+	meow(){
+		console.log('Meow');
+	}
+	eat(food){
+		with(food){
+			console.log('I just ate ' + type);
+		}
 	}
 }
 
-car.vroom();
+let ourCat = new Cat();
+
+ourCat.meow();
+ourCat.eat({ type: 'Tuna' });
