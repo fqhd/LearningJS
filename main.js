6cea0d7679a8c27d0025a46b111cde9416043e8f
fqhd <fahd31256@gmail.com>
2021-09-25 07:18:59 +0100

    Function data privacy

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-let greet = () => {
-	let displayName = () => {
-		console.log('Jaden');
+function sum(){
+	let a = 0;
+	function increaseSum(){
+		return a++;
 	}
-
-	return displayName;
+	return increaseSum;
 }
 
-greet()();
+let incrementingFunction = sum();
+console.log(incrementingFunction());
+console.log(incrementingFunction());
+console.log(incrementingFunction());
+console.log(incrementingFunction());
+console.log(incrementingFunction());
+console.log(incrementingFunction());
+let anotherIncrementingFunction = sum();
+console.log(anotherIncrementingFunction());
+console.log(anotherIncrementingFunction());
+console.log(anotherIncrementingFunction());
+console.log(anotherIncrementingFunction());
+console.log(anotherIncrementingFunction());
+console.log(incrementingFunction());
