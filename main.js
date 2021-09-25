a60f387f129662750754fcadbafe07a6f4d8f7b4
fqhd <fahd31256@gmail.com>
2021-09-25 16:13:48 +0100

    this keyword in function callbacks

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 const myObject = {
-	age: 32,
-	myFunction: function() {
-		console.log(this);
-	},
-	hit: () => {
-		console.log(this); // This will not work because this doesn't exist inside arrow functions
+	name: 'Bob',
+	kill: undefined,
+	initKillFunc(killFunc){
+		this.kill = killFunc;
 	}
 };
 
-myObject.hit.bind(myObject);// This will not work because hit is an arrow function
-
-function testFunction(){
-	myObject.myFunction();
-	myObject.hit();
+function ourFunc(){
+	console.log(this.name);
 }
-testFunction();
+
+myObject.initKillFunc(ourFunc);
+
+myObject.kill();
+
+/* 
+	The .bind here is very important because with it, we are just sending a function reference
+	And the problem with that is that when the function is going to be called(after 1 second),
+	There will be no Object scope for the *this* keyword to point to. So *this* will be
+	undefined. So a solution to this issue is to pass the function with a *this* property
+	instead of having *this* being dynamically calculated.
+*/
+
+setTimeout(myObject.kill.bind(myObject), 1000);
+
+let ourFuncRef = myObject.kill;
+
+ourFuncRef();
