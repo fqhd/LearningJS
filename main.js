36ce53201cd58442458bdd8919ee54758a1240be
fqhd <fahd31256@gmail.com>
2021-09-25 20:58:19 +0100

    Factory functions

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-let mySymbol = Symbol.for(9);
-let myOtherSymbol = Symbol.for(9);
-console.log(mySymbol);
-console.log(myOtherSymbol);
-
-if(mySymbol === myOtherSymbol){
-	console.log('They are equal!');
+let factoryFunction = () => {
+	return {
+		age: 30,
+		myFunction: function(){
+			console.log('This is my factory function');
+		}
+	}
 }
+
+let obj = factoryFunction(); // Cannot use new
+let mySymbol = Symbol(); // Cannot use new the same way we can't use new for factoryFunction()
+
+obj.myFunction();
