9cf478ac86dd71229834aa12a016d7b9c8438364
fqhd <fahd31256@gmail.com>
2021-09-28 08:06:37 +0100

    Getters and setters

--- main.js
+++ main.js
@@ -5,19 +5,23 @@ class Animal {
 }
 
 class Dog extends Animal {
+	#age = 35;
 	constructor(name, type){
 		super(type);
 		this.name = name;
+		this.#age = 46;
 	}
 	printType(){
 		console.log(this.name + ' is a ' + this.type);
+	}
+	get age(){
+		return this.#age;
+	}
+	set age(age){
+		this.#age = age;
 	}
 }
 
 let myDog = new Dog('George', 'Lion');
 
-myDog.printType();
-
-myDog.type = 'Zebra';
-
-myDog.printType();
+console.log(myDog.age);
