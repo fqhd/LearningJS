dab69da1da8213035aa77ad5e7bdde384e04582f
fqhd <fahd31256@gmail.com>
2021-10-08 08:40:46 -0700

    Passing primitives by value

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-const myApple = {
-	age: 45,
-	name: 'Fahd'
-};
-
-function myFunction({name, age}){
-	console.log('Name: ' + name);
-	console.log('Age: ' + age);
+class Apple {
+	#age;
+	constructor(name, age){ // Constructor(called only when using new)
+		this.#age = age; // Private variable
+		this.name = name;
+	}
+	get age(){
+		return this.#age;
+	}
 }
 
-myFunction(myApple);
+let myName = 'George';
+let myApple = new Apple(myName, 12);
+console.log(myApple.name); // Prints George
+
+myName = 'Fahd'; // Changing my name variable
+
+console.log(myApple.name); // Still prints George
+
+// It doesn't change because only objects can be passed by reference
