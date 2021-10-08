d780ac4aaa60539008383df21cc28c63fce4ee57
fqhd <fahd31256@gmail.com>
2021-10-08 08:54:08 -0700

    Array.filter()

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 class Apple {
-	#age;
 	constructor(name, age){ // Constructor(called only when using new)
-		this.#age = age; // Private variable
+		this.age = age; // Private variable
 		this.name = name;
-	}
-	get age(){
-		return this.#age;
 	}
 }
 
-let myName = {name: 'George'}; // Name is now an object
-let myApple = new Apple(myName, 12);
-console.log(myApple.name.name); // Prints George
+let myArray = [
+	new Apple('George', 20),
+	new Apple('George', 30),
+	new Apple('George', 60),
+	new Apple('George', 90),
+];
 
-myName.name = 'Fahd'; // Changing the name variable inside the myName object
+let getApple = apple => {
+	return apple.age > 30;
+}
 
-console.log(myApple.name); // Prints Fahd now because we have changed the name variable under the object
+let filteredApples = myArray.filter(apple => {
+	return apple.age < 50; 
+});
 
-// Since we are passing in an object to myApple now, the value of name can be controlled outside of the
-// Apple scope because it is being passed by reference since it is an object and objects are always
-// passed by reference.
+for(let i of filteredApples){
+	console.log(i);
+}
